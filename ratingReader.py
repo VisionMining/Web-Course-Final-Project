fa79a6301d42ca630f6eb6fcfab1c833a817b698
Gangdu <gangduliu@outlook.com>
2019-11-01 11:37:32 +0800

    update ratingReader

--- ratingReader.py
+++ ratingReader.py
@@ -17,21 +17,25 @@ class RatingReader:
         """
         self.rand = random.Random(0)
         self.k_fold = k_fold
-        self.rating_file_name = rating_file_name
+        self.__rating_file_name = rating_file_name
+
         self.user_num = 0
         self.item_num = 0
-        self.user_adj_lists = {}
-        self.construct_user_adj_lists_by_rating_file()
-        self.train_set_user_adj = [None] * self.user_num
-        self.train_set_item_adj = [None] * self.item_num
-        self.test_set = [None] * self.user_num
+        self.__user_adj_lists = {}
+        self.__construct_user_adj_lists_by_rating_file()
+
+        self.__training_user_adj = [None] * self.user_num
+        self.__training_item_adj = [None] * self.item_num
+        self.__training_set = []
+        self.__test_set = []
+        self.__split_train_test_set_by_user()
+        self.__construct_item_adj_lists_by_training_user_adj()
+
         self.user_max_rating_num = 0
         self.user_min_rating_num = 1000000
-        self.split_train_test_set_by_user()
-        self.construct_item_adj_lists_by_train_set_user_adj()
 
-    def construct_user_adj_lists_by_rating_file(self):
-        with open(self.rating_file_name, mode='r', encoding='utf-8', errors='replace') as csv_file:
+    def __construct_user_adj_lists_by_rating_file(self):
+        with open(self.__rating_file_name, mode='r', encoding='utf-8', errors='replace') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             header = next(csv_reader, None)
             for row in csv_reader:
@@ -40,50 +44,61 @@ class RatingReader:
                 rating = float(row[2])
                 self.user_num = max(self.user_num, user_id + 1)
                 self.item_num = max(self.item_num, item_id + 1)
-                if self.user_adj_lists.get(user_id, None) is None:
-                    self.user_adj_lists[user_id] = [(item_id, rating)]
+                if user_id in self.__user_adj_lists:
+                    self.__user_adj_lists[user_id].append((item_id, rating))
                 else:
-                    self.user_adj_lists[user_id].append((item_id, rating))
+                    self.__user_adj_lists[user_id] = [(item_id, rating)]
             print(header)
 
-    def split_train_test_set_by_user(self):
-        for user_id, item_rating_list in self.user_adj_lists.items():
+    def __split_train_test_set_by_user(self):
+        for user_id, item_rating_list in self.__user_adj_lists.items():
             self.rand.shuffle(item_rating_list)
             total_size = len(item_rating_list)
             split_size = total_size // self.k_fold
-            self.train_set_user_adj[user_id] = item_rating_list[split_size:total_size]
-            self.test_set[user_id] = item_rating_list[0:split_size]
-            self.user_max_rating_num = max(self.user_max_rating_num, total_size)
-            self.user_min_rating_num = min(self.user_min_rating_num, total_size)
+            self.__training_user_adj[user_id] = dict(item_rating_list[split_size:total_size])
+            for item_id, rating in item_rating_list[0:split_size]:
+                self.__test_set.append((user_id, item_id, rating))
 
-    def construct_item_adj_lists_by_train_set_user_adj(self):
+    def __construct_item_adj_lists_by_training_user_adj(self):
         for user_id in range(self.user_num):
-            item_rating_list = self.train_set_user_adj[user_id]
-            for item_id, rating in item_rating_list:
-                if self.train_set_item_adj[item_id] is None:
-                    self.train_set_item_adj[item_id] = [(user_id, rating)]
+            for item_id, rating in self.__training_user_adj[user_id].items():
+                if self.__training_item_adj[item_id] is None:
+                    self.__training_item_adj[item_id] = {user_id: rating}
                 else:
-                    self.train_set_item_adj[item_id].append((user_id, rating))
+                    self.__training_item_adj[item_id][user_id] = rating
+
+    def contain_rating(self, user_id, item_id):
+        return item_id in self.__training_user_adj[user_id]
 
     def get_user_rating_vector(self, user_id):
         user_vector = np.zeros(self.item_num, dtype = float)
-        for item_id, rating in self.train_set_user_adj[user_id]:
+        for item_id, rating in self.__training_user_adj[user_id].items():
             user_vector[item_id] = rating
         return user_vector
 
     def get_item_rating_vector(self, item_id):
         item_vector = np.zeros(self.user_num, dtype = float)
-        for user_id, rating in self.train_set_item_adj[item_id]:
+        for user_id, rating in self.__training_item_adj[item_id].items():
             item_vector[user_id] = rating
         return item_vector
 
+    def get_training_set(self):
+        if len(self.__training_set) == 0:
+            for user_id in range(self.user_num):
+                for item_id, rating in self.__training_user_adj[user_id].items():
+                    self.__training_set.append((user_id, item_id, rating))
+        return self.__training_set
+
+    def get_test_set(self):
+        return self.__test_set
+
     def __str__(self):
         return f'user num: {self.user_num}, itme num: {self.item_num}\n' \
-               f'user max, min rating num: {self.user_max_rating_num, self.user_min_rating_num}\n' \
-               f'user adj lists: {self.user_adj_lists[1002]}\n' \
-               f'train set user: {self.train_set_user_adj[1002]}\n' \
-               f'train set item: {self.train_set_item_adj[48229]}\n' \
-               f'test set: {self.test_set[1002]}\n'
+            f'user adj lists: {self.__user_adj_lists[6998]}\n' \
+            f'train set user: {self.__training_user_adj[6998]}\n' \
+            f'train set item: {self.__training_item_adj[73256]}\n' \
+            f'train set: {self.get_training_set()[0:3]}\n' \
+            f'test set: {self.get_test_set()[0:3]}\n' \
 
 
 if __name__ == "__main__":
