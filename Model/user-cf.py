aa49aa708e84981b9982a3e1b9baad60c719fd75
Gangdu Liu <gdliu.618@gmail.com>
2019-11-02 15:41:16 +0800

    update user-cf and utility

--- Model/user-cf.py
+++ Model/user-cf.py
@@ -1,39 +1,40 @@
 import numpy as np
-from scipy.sparse import coo_matrix
-from utility import adjust_cosine,cosine_based,correlation_based
+import sys
 
-train_split = np.loadtxt('../data/ml_ratings_trainset.txt')
-row=train_split[:,0]
-col=train_split[:,1]
-data=train_split[:,2]
-# num_user=1509
-# num_item=2072
-num_user=944
-num_item=1683
+sys.path.append('..')
 
-# 去用户中心化，实验结果这在ub的三种相似度计算中是效果最好的。
-ui_matrix = coo_matrix((data, (row,col)), shape=(num_user,num_item)).toarray()
-usercf=userCF(ui_matrix, num_user,num_item, correlation_based)
-test_split = np.loadtxt('../data/ml_ratings_testset.txt')
-rmse,_mae,_=usercf.predict_model(test_split)
-# print(rmse)
-print("correlation_based:")
-print(_mae)
+from ratingReader import RatingReader
+from utility import adjust_cosine, cosine_based, correlation_based
 
-# 余弦相似度
-ui_matrix = coo_matrix((data, (row,col)), shape=(num_user,num_item)).toarray()
-usercf=userCF(ui_matrix, num_user,num_item, cosine_based)
-test_split = np.loadtxt('../data/ml_ratings_testset.txt')
-rmse,_mae,_=usercf.predict_model(test_split)
-# print(rmse)
-print("cosine_based:")
-print(_mae)
 
-# 去物品中心化
-ui_matrix = coo_matrix((data, (row,col)), shape=(num_user,num_item)).toarray()
-usercf=userCF(ui_matrix, num_user,num_item, adjust_cosine)
-test_split = np.loadtxt('../data/ml_ratings_testset.txt')
-rmse,_mae,_=usercf.predict_model(test_split)
-# print(rmse)
-print("adjust_cosine:")
-print(_mae)
+class UserCf:
+
+    def __init__(self, rating_reader, k_neighbor=30):
+        """
+
+        :param rating_reader: rating reader object
+        :type rating_reader: RatingReader
+        :param k_neighbor: k most similar user to predict rating
+        :type k_neighbor: int
+        """
+        self.rating_reader = rating_reader
+        self.k_neighbor = k_neighbor
+        self.user_num = rating_reader.get_user_num()
+
+    def __compute_similarity(self, ):
+        pass
+
+    def __predict_one(self, user, item):
+        u1 = self.rating_reader.get_user_rating_vector(user)
+        for user_other in range(self.user_num):
+            if self.rating_reader.contains_rating(user_other, item):
+                u2 = self.rating_reader.get_user_rating_vector(user_other)
+
+    def predict(self):
+        for user, item, rating in self.rating_reader.get_training_set():
+            pass
+
+if __name__ == '__main__':
+    rating_file = 'Processed-Data/mapped-ratings.csv'
+    rr = RatingReader(rating_file, k_fold=5)
+    user_cf = UserCf(rr, k_neighbor=30)
--- utility.py
+++ utility.py
@@ -1,66 +1,34 @@
 import numpy as np
-import random
 import math
-import time
-from numpy.linalg import norm
-from tqdm import tqdm
-from scipy.sparse import coo_matrix
+from numpy import linalg as LA
 
 
-def cosine_based(data, num1, num2):
+def cosine_based(vector1, vector2):
     """
-    计算最基本的余弦相似度，
-    如果是计算用户相似度，则data矩阵以用户为行，项目为列，
-    如果是计算项目相似度则相反
-    :param data:
-    :type data:
-    :param num1:
-    :type num1:
-    :param num2:
-    :type num2:
-    :return:
-    :rtype:
+    Compute two vectors cosine based similarity.
+    :param vector1: 1-D array
+    :type vector1: list
+    :param vector2: 1-D array
+    :type vector2: ndarray
+    :return: similarity score
+    :rtype: float
     """
-    print("beginsim")
-    #data=data.toarray()
-   
-    dataT=data.T
-    nn=np.matmul(data[0],dataT)
-    for i in range(1,num1):
-        nn=np.vstack((nn,np.matmul(data[i],dataT)))
-    #print(nn)
-    normal=np.empty(num1)
-    for i in range(num1):
-        normal[i]=norm(data[i])    
-    normalT=normal[:, np.newaxis]
-    normal=normal[np.newaxis,:]
-    nor=np.matmul(normalT,normal)+0.0000001
-    similarity=np.divide(nn,nor)
-    return similarity-np.identity(num1)
+    similarity = np.dot(vector1, vector2) / (LA.norm(vector1, 2) * LA.norm(vector2, 2))
+    return similarity
 
 
-    # 减去用户评分均值
-    #如果是计算用户相似度，则data矩阵以用户为行，项目为列，
-    #如果是计算项目相似度则相反
-def correlation_based(ui_matrix,users,items):
-    for i in range(users):
-        user=ui_matrix[i,:]
-        whl=(user!=0)
-        useri=user[whl]
-        ui_matrix[i,:][whl]=ui_matrix[i,:][whl]-np.average(useri)
-    return cosine_based(ui_matrix,users,items)
+def correlation_based(ui_matrix, users, items):
+    pass
 
-    # 减去物品评分均值
-    #如果是计算用户相似度，则data矩阵以用户为行，项目为列，
-    #如果是计算项目相似度则相反
-def adjust_cosine(ui_matrix,users,items): 
+
+def adjust_cosine(ui_matrix, users, items):
     for i in range(items):
-        user=ui_matrix[:,i]
-        whl=(user!=0)
-        useri=user[whl]
-        ui_matrix[:,i][whl]=ui_matrix[:,i][whl]-np.average(useri)
-    
-    return cosine_based(ui_matrix,users,items)
+        user = ui_matrix[:, i]
+        whl = (user != 0)
+        useri = user[whl]
+        ui_matrix[:, i][whl] = ui_matrix[:, i][whl] - np.average(useri)
+
+    return cosine_based(ui_matrix, users, items)
 
 
 def MAE(res):
@@ -78,7 +46,7 @@ def RMSE(res):
     error = 0
     count = 0
     for entry in res:
-        error += abs(entry[2] - entry[3])**2
+        error += abs(entry[2] - entry[3]) ** 2
         count += 1
     if count == 0:
         return error
@@ -97,10 +65,18 @@ def checkRatingBoundary(prediction, min_val, max_val):
 def denormalize(rating, minVal, maxVal):
     return minVal + (rating - 0.01) * (maxVal - minVal)
 
+
 def normalize(rating, minVal, maxVal):
-    return (rating-minVal)/(maxVal-minVal)+0.01
+    return (rating - minVal) / (maxVal - minVal) + 0.01
+
 
 def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
 
 
+if __name__ == '__main__':
+    a = [1, 2, 3]
+    b = np.array(a)
+    b[0] = 4
+    print([{}] * 5)
+    print(b)
