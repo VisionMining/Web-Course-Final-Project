3139643a0a24250103029438c10a982043836c8f
Gangdu Liu <gdliu.618@gmail.com>
2019-11-04 20:02:50 +0800

    add some function

--- utility.py
+++ utility.py
@@ -17,7 +17,7 @@ def cosine_based(vector1, vector2):
     return similarity
 
 
-def MAE(predict_value, true_value):
+def mae(predict_value, true_value):
     error = 0
     count = 0
     for i in range(len(predict_value)):
@@ -28,7 +28,7 @@ def MAE(predict_value, true_value):
     return float(error) / count
 
 
-def RMSE(predict_value, true_value):
+def rmse(predict_value, true_value):
     error = 0
     count = 0
     for i in range(len(predict_value)):
@@ -37,27 +37,6 @@ def RMSE(predict_value, true_value):
     if count == 0:
         return error
     return math.sqrt(float(error) / count)
-
-
-def checkRatingBoundary(prediction, min_val, max_val):
-    if prediction > max_val:
-        return max_val
-    elif prediction < min_val:
-        return min_val
-    else:
-        return round(prediction, 3)
-
-
-def denormalize(rating, minVal, maxVal):
-    return minVal + (rating - 0.01) * (maxVal - minVal)
-
-
-def normalize(rating, minVal, maxVal):
-    return (rating - minVal) / (maxVal - minVal) + 0.01
-
-
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
 
 
 if __name__ == '__main__':
