3a4c6cf28c646efa6769377e7d1020d851a3b094
Gangdu Liu <gdliu.618@gmail.com>
2019-11-04 09:26:20 +0800

    change preprocess code make it more readable

--- preprocess.py
+++ preprocess.py
@@ -7,14 +7,17 @@ def filter_rating_by_zero(input_file_name, output_file_name):
         csv_reader = csv.reader(csv_file, delimiter=';')
         header = next(csv_reader, None)
         for row in csv_reader:
-            if row[2] != '0':
-                filtered_ratings.append([row[0], row[1], row[2]])
+            use_id = row[0]
+            item_id = row[1]
+            rating = row[2]
+            if rating != '0':
+                filtered_ratings.append([use_id, item_id, rating])
         print(header)
     with open(output_file_name, mode='w', encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(['user-id', 'item-id', 'rating'])
-        for rating in filtered_ratings:
-            writer.writerow(rating)
+        for triplet in filtered_ratings:
+            writer.writerow(triplet)
     print('filter rating data by zero')
 
 
@@ -24,14 +27,16 @@ def compute_save_user_map(input_file_name, output_file_name):
         csv_reader = csv.reader(csv_file, delimiter=';')
         header = next(csv_reader, None)
         for row in csv_reader:
-            user_set.add(row[0])
+            user_id = row[0]
+            user_set.add(user_id)
         print(header)
     user_list = list(user_set)
     with open(output_file_name, mode='w', encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(['original-user-id', 'mapped-user-id'])
-        for i in range(len(user_list)):
-            writer.writerow([user_list[i], i])
+        for mapped_user_id in range(len(user_list)):
+            original_user_id = user_list[mapped_user_id]
+            writer.writerow([original_user_id, mapped_user_id])
     print('compute and save user map complete')
 
 
@@ -41,14 +46,16 @@ def compute_save_item_map(input_file_name, output_file_name):
         csv_reader = csv.reader(csv_file, delimiter=';')
         header = next(csv_reader, None)
         for row in csv_reader:
-            item_set.add(row[1])
+            item_id = row[1]
+            item_set.add(item_id)
         print(header)
     item_list = list(item_set)
     with open(output_file_name, mode='w', encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(['original-item-id', 'mapped-item-id'])
-        for i in range(len(item_list)):
-            writer.writerow([item_list[i], i])
+        for mapped_item_id in range(len(item_list)):
+            original_item_id = item_list[mapped_item_id]
+            writer.writerow([original_item_id, mapped_item_id])
     print('compute and save item map complete')
 
 
@@ -67,14 +74,18 @@ def get_user_item_map(user_map_file_name, item_map_file_name):
         csv_reader = csv.reader(csv_file, delimiter=';')
         header = next(csv_reader, None)
         for row in csv_reader:
-            user_map_dict[row[0]] = row[1]
+            original_user_id = row[0]
+            mapped_user_id = row[1]
+            user_map_dict[original_user_id] = mapped_user_id
         print(header)
     item_map_dict = {}
     with open(item_map_file_name, mode='r', encoding='utf-8', errors='replace') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         header = next(csv_reader, None)
         for row in csv_reader:
-            item_map_dict[row[0]] = row[1]
+            original_item_id = row[0]
+            mapped_item_id = row[1]
+            item_map_dict[original_item_id] = mapped_item_id
         print(header)
     return user_map_dict, item_map_dict
 
@@ -85,8 +96,12 @@ def compute_save_mapped_rating(user_map_dict, item_map_dict, input_file_name, ou
         csv_reader = csv.reader(csv_file, delimiter=';')
         header = next(csv_reader, None)
         for row in csv_reader:
-            mapped_ratings.append([user_map_dict[row[0]], item_map_dict[row[1]], row[2]])
+            mapped_user_id = int(user_map_dict[row[0]])
+            mapped_item_id = int(item_map_dict[row[1]])
+            rating = row[2]
+            mapped_ratings.append([mapped_user_id, mapped_item_id, rating])
         print(header)
+    mapped_ratings = sorted(mapped_ratings, key=lambda x: (x[0], x[1]))
     with open(output_file_name, mode='w', encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(['mapped-user-id', 'mapped-item-id', 'rating'])
