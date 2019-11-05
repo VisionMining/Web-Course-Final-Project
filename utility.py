a2a034c359f10193f80970bc58add800ee4f0fa4
Gangdu Liu <gdliu.618@gmail.com>
2019-11-05 19:38:08 +0800

    delete useless function

--- utility.py
+++ utility.py
@@ -1,20 +1,4 @@
 import numpy as np
-import math
-from numpy import linalg as LA
-
-
-def cosine_based(vector1, vector2):
-    """
-    Compute two vectors cosine based similarity.
-    :param vector1: 1-D array
-    :type vector1: list
-    :param vector2: 1-D array
-    :type vector2: ndarray
-    :return: similarity score
-    :rtype: float
-    """
-    similarity = np.dot(vector1, vector2) / (LA.norm(vector1, 2) * LA.norm(vector2, 2))
-    return similarity
 
 
 def mae(predict_value, true_value):
